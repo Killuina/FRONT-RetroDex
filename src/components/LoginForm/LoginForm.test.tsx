a0dce259c4226d5578e8d29e0229b9a067bb5dd0
Cristina <cristinajmartin98@gmail.com>
2023-03-09 19:21:44 +0100

    Add test for LoginForm functionality

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import LoginForm from "./LoginForm";
 
 describe("Given the LoginForm component", () => {
@@ -19,6 +20,66 @@ describe("Given the LoginForm component", () => {
       const passwordField = screen.getByLabelText(expectedLabel);
 
       expect(passwordField).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user writes on Username's input field", () => {
+    test("Then it should change the value of username's input", async () => {
+      const usernameLabel = "Username";
+      const expectedInputValue = "manolo";
+      render(<LoginForm />);
+
+      const usernameInput = screen.getByLabelText(usernameLabel);
+
+      await act(
+        async () => await userEvent.type(usernameInput, expectedInputValue)
+      );
+
+      expect(usernameInput).toHaveValue(expectedInputValue);
+    });
+  });
+
+  describe("When the user writes on Password's input field", () => {
+    test("Then it should change the value of password's input", async () => {
+      const passwordLabel = "Password";
+      const expectedInputValue = "manolo1234";
+      render(<LoginForm />);
+
+      const usernameInput = screen.getByLabelText(passwordLabel);
+
+      await act(
+        async () => await userEvent.type(usernameInput, expectedInputValue)
+      );
+
+      expect(usernameInput).toHaveValue(expectedInputValue);
+    });
+  });
+
+  describe("When the user clicks 'Sign in' button", () => {
+    test("Then it should reset the value of username's input field", async () => {
+      const usernameLabel = "Username";
+      const expectedInputValue = "";
+      render(<LoginForm />);
+
+      const usernameInput = screen.getByLabelText(usernameLabel);
+      const loginButton = screen.getByRole("button", { name: "Sign in" });
+
+      await act(async () => await userEvent.click(loginButton));
+
+      expect(usernameInput).toHaveValue(expectedInputValue);
+    });
+
+    test("Then it should reset the value of password's input field", async () => {
+      const passwordLabel = "Password";
+      const expectedInputValue = "";
+      render(<LoginForm />);
+
+      const passwordInput = screen.getByLabelText(passwordLabel);
+      const loginButton = screen.getByRole("button", { name: "Sign in" });
+
+      await act(async () => await userEvent.click(loginButton));
+
+      expect(passwordInput).toHaveValue(expectedInputValue);
     });
   });
 });
