6a546d0276778a7f3087e7196ae6efbe65b01685
Cristina <cristinajmartin98@gmail.com>
2023-03-09 16:39:34 +0100

    Add Chakra UI Login form with basic logic

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,16 +1,50 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import LoginFormStyled from "./LoginFormStyled";
+
 const LoginForm = (): JSX.Element => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isUsernameFieldEmpty = username === "";
+  const isPasswordFieldEmpty = password === "";
+
   return (
-    <form className="login-form">
-      <label htmlFor="username" className="login-form__field">
-        Username
-        <input type="text" autoComplete="off" id="username"></input>
-      </label>
-      <label htmlFor="password" className="login-form__field">
-        Password
-      </label>
-      <input type="password" id="password" />
-      <button>Sign in</button>
-    </form>
+    <LoginFormStyled>
+      <FormControl
+        className="login-form"
+        isInvalid={isUsernameFieldEmpty}
+        fontFamily="form"
+        bg="primary.100"
+      >
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input id="username" type="text" bg="white" />
+
+        {isUsernameFieldEmpty ? (
+          <FormErrorMessage className="form-error-message">
+            Username is required
+          </FormErrorMessage>
+        ) : (
+          ""
+        )}
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input id="password" type="password" bg="white" autoComplete="on" />
+        {isPasswordFieldEmpty ? (
+          <FormErrorMessage className="form-error-message">
+            Password is required
+          </FormErrorMessage>
+        ) : (
+          ""
+        )}
+        <Button type="submit">Sign in</Button>
+      </FormControl>
+    </LoginFormStyled>
   );
 };
 
