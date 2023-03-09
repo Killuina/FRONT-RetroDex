dad0c9c6e3f665caaa2e4e046c16d888b0c355f6
Cristina <cristinajmartin98@gmail.com>
2023-03-09 19:08:56 +0100

    Add LoginForm functionality

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,19 +4,38 @@ import {
   Input,
   FormErrorMessage,
   Button,
+  FormHelperText,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import LoginFormStyled from "./LoginFormStyled";
 
 const LoginForm = (): JSX.Element => {
-  const [username] = useState("");
-  const [password] = useState("");
+  const [userLoginCredentials, setUserLoginCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
-  const isUsernameFieldEmpty = username === "";
-  const isPasswordFieldEmpty = password === "";
+  const isUsernameFieldEmpty =
+    userLoginCredentials.username === "" ||
+    userLoginCredentials.password === "";
+
+  const handleUserLoginCredentials = ({
+    target: { id, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setUserLoginCredentials({
+      ...userLoginCredentials,
+      [id]: value,
+    });
+  };
+
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setUserLoginCredentials({ username: "", password: "" });
+  };
 
   return (
-    <LoginFormStyled>
+    <LoginFormStyled onSubmit={onSubmitHandler}>
       <FormControl
         className="login-form"
         isInvalid={isUsernameFieldEmpty}
@@ -24,23 +43,40 @@ const LoginForm = (): JSX.Element => {
         bg="primary.100"
       >
         <FormLabel htmlFor="username">Username</FormLabel>
-        <Input id="username" type="text" bg="white" />
+        <Input
+          id="username"
+          type="text"
+          bg="white"
+          value={userLoginCredentials.username}
+          onChange={handleUserLoginCredentials}
+        />
 
         {isUsernameFieldEmpty ? (
-          <FormErrorMessage className="form-error-message">
+          <FormErrorMessage className="form-message">
             Username is required
           </FormErrorMessage>
         ) : (
-          ""
+          <FormHelperText className="form-message">
+            Your username is the best
+          </FormHelperText>
         )}
         <FormLabel htmlFor="password">Password</FormLabel>
-        <Input id="password" type="password" bg="white" autoComplete="on" />
-        {isPasswordFieldEmpty ? (
-          <FormErrorMessage className="form-error-message">
+        <Input
+          id="password"
+          type="password"
+          bg="white"
+          autoComplete="on"
+          value={userLoginCredentials.password}
+          onChange={handleUserLoginCredentials}
+        />
+        {isUsernameFieldEmpty ? (
+          <FormErrorMessage className="form-message">
             Password is required
           </FormErrorMessage>
         ) : (
-          ""
+          <FormHelperText className="form-message">
+            Your password is even better
+          </FormHelperText>
         )}
         <Button type="submit">Sign in</Button>
       </FormControl>
