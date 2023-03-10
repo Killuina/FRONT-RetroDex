fe4c6dd89cec73d3fd332c12f55f8f6a1c616318
Cristina <cristinajmartin98@gmail.com>
2023-03-10 13:50:36 +0100

    Add fetch pollyfill and change nextjs alias

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,6 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   collectCoverageFrom: [
     "src/**/*.ts",
