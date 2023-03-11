a1466264560095b9736bb9f5ada6d2c63b495f84
Cristina <cristinajmartin98@gmail.com>
2023-03-11 18:53:18 +0100

    Add test ofr set and unSet is Error actions

--- __tests__/pagesTests/uiSlice.test.tsx
+++ __tests__/pagesTests/uiSlice.test.tsx
@@ -1,46 +1,60 @@
 import { UiState } from "../../src/store/features/uiSlice/types";
 import {
+  setIsErrorActionCreator,
   setIsLoadingActionCreator,
   uiReducer,
+  unsetIsErrorActionCreator,
   unsetIsLoadingActionCreator,
 } from "../../src/store/features/uiSlice/uiSlice";
+
+const currentUiState: UiState = {
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+};
 
 describe("Given the uiSliceReducer reducer", () => {
   describe("When it receives the action to set isLoading", () => {
     test("Then it should set isLoading to true", () => {
-      const currentUiState: UiState = {
-        isLoading: false,
-      };
-
-      const expectedUiState: UiState = {
-        isLoading: true,
-      };
-
       const uiState: UiState = uiReducer(
         currentUiState,
         setIsLoadingActionCreator()
       );
-
-      expect(uiState).toStrictEqual(expectedUiState);
+      8;
+      expect(uiState).toHaveProperty("isLoading", true);
     });
   });
 
   describe("When it receives the action to unset isLoading", () => {
     test("Then it should set isLoading to false", () => {
-      const currentUiState: UiState = {
-        isLoading: true,
-      };
-
-      const expectedUiState: UiState = {
-        isLoading: false,
-      };
-
       const uiState: UiState = uiReducer(
         currentUiState,
         unsetIsLoadingActionCreator()
       );
 
-      expect(uiState).toStrictEqual(expectedUiState);
+      expect(uiState).toHaveProperty("isLoading", false);
+    });
+  });
+
+  describe("When it receives the action to set isError", () => {
+    test("Then it should set isError to true", () => {
+      const uiState: UiState = uiReducer(
+        currentUiState,
+        setIsErrorActionCreator()
+      );
+
+      expect(uiState).toHaveProperty("isError", true);
+    });
+  });
+
+  describe("When it receives the action to unset isError", () => {
+    test("Then it should set isError to false", () => {
+      const uiState: UiState = uiReducer(
+        currentUiState,
+        unsetIsErrorActionCreator()
+      );
+
+      expect(uiState).toHaveProperty("isError", false);
     });
   });
 });
