f3f753d0ecdfbd179a1ff9b41946100d97941a85
Cristina <cristinajmartin98@gmail.com>
2023-03-13 10:43:02 +0100

    Fix fonts not prerendering by declaring then on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { IBM_Plex_Mono, VT323 } from "next/font/google";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
+import Layout from "../components/Layout/Layout";
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
 import GlobalStyles from "../GlobalStyles";
 import { store } from "../store/store";
 import { mainTheme } from "../styles/mainTheme";
+
+export const mainFont = VT323({ subsets: ["latin"], weight: "400" });
+
+export const secondaryFont = IBM_Plex_Mono({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const App = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
@@ -18,7 +27,9 @@ const App = ({ Component, pageProps, router }: AppProps): JSX.Element => {
         <ThemeProvider theme={mainTheme}>
           <GlobalStyles />
           <ProtectedRoute router={router}>
-            <Component {...pageProps} />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
           </ProtectedRoute>
         </ThemeProvider>
       </Provider>
--- src/styles/fonts/googleFonts.ts
+++
@@ -1,8 +0,0 @@
-import { IBM_Plex_Mono, VT323 } from "next/font/google";
-
-export const mainFont = VT323({ subsets: ["latin"], weight: "400" });
-
-export const secondaryFont = IBM_Plex_Mono({
-  subsets: ["latin"],
-  weight: "400",
-});
