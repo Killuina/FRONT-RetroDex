9a10712cc7cd78d4d3f2d7fbdbaee7c9f45f668b
Cristina <cristinajmartin98@gmail.com>
2023-03-15 10:14:56 +0100

    Refactor Card styling

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -10,7 +10,7 @@ const PokemonCard = ({
   userPokemon: { name, imageUrl, types },
 }: PokemonCardProps): JSX.Element => {
   return (
-    <PokemonCardStyled pokemonType={types[0]} className="pokemon-card">
+    <PokemonCardStyled className={`pokemon-card ${types[0]}`}>
       <Image
         src={`${imageUrl}`}
         alt={`${name}`}
@@ -18,7 +18,7 @@ const PokemonCard = ({
         height="130"
         priority={true}
       />
-      <h2 className="pokemon-card__name">{name}</h2>
+      <h3 className="pokemon-card__name">{name}</h3>
       <span className="pokemon-card__types">Type:{types.join("/")}</span>
       <div className="pokemon-card__buttons">
         <button className="pokemon-card__button" aria-label="edit pokemon">
