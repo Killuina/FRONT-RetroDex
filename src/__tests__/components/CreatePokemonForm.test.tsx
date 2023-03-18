1f5b7716e10ac77e4165a227886633d30a26b5a2
Cristina <cristinajmartin98@gmail.com>
2023-03-18 19:59:35 +0100

    Add test for create pokemon formm functionality

--- src/__tests__/components/CreatePokemonForm.test.tsx
+++ src/__tests__/components/CreatePokemonForm.test.tsx
@@ -1,4 +1,5 @@
-import { screen } from "@testing-library/react";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CreatePokemonForm from "../../components/CreatePokemonForm/CreatePokemonForm";
 import renderWithProviders from "../../utils/testUtils/renderWithProviders";
 
@@ -87,6 +88,120 @@ describe("Given the CreatePokemonForm component", () => {
       });
 
       expect(createPokemonButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user writes on name's field", () => {
+    test("Then it should change the value of name's field", async () => {
+      const nameLabel = "Name";
+      const expectedFieldValue = "manolo";
+      renderWithProviders(<CreatePokemonForm />);
+
+      const nameField = screen.getByLabelText(nameLabel);
+
+      await waitFor(
+        async () => await userEvent.type(nameField, expectedFieldValue)
+      );
+
+      expect(nameField).toHaveValue(expectedFieldValue);
+    });
+  });
+
+  describe("When the user writes on name's field", () => {
+    test("Then it should change the value of name's field", async () => {
+      const nameLabel = "Name";
+      const expectedFieldValue = "manolo";
+      renderWithProviders(<CreatePokemonForm />);
+
+      const nameField = screen.getByLabelText(nameLabel);
+
+      await waitFor(
+        async () => await userEvent.type(nameField, expectedFieldValue)
+      );
+
+      expect(nameField).toHaveValue(expectedFieldValue);
+    });
+  });
+
+  describe("When the user writes on ability's field", () => {
+    test("Then it should change the value of ability's field", async () => {
+      const abilityLabel = "Ability";
+      const expectedFieldValue = "Pesao";
+      renderWithProviders(<CreatePokemonForm />);
+
+      const abilityField = screen.getByLabelText(abilityLabel);
+
+      await waitFor(
+        async () => await userEvent.type(abilityField, expectedFieldValue)
+      );
+
+      expect(abilityField).toHaveValue(expectedFieldValue);
+    });
+  });
+
+  describe("When the user writes on height's field", () => {
+    test("Then it should change the value of height's field", async () => {
+      const heightLabel = "Height";
+      const expectedFieldValue = "23";
+      renderWithProviders(<CreatePokemonForm />);
+
+      const heightField = screen.getByLabelText(heightLabel);
+
+      await waitFor(
+        async () => await userEvent.type(heightField, expectedFieldValue)
+      );
+
+      expect(heightField).toHaveValue(expectedFieldValue);
+    });
+  });
+
+  describe("When the user writes on weight's field", () => {
+    test("Then it should change the value of weight's field", async () => {
+      const weightLabel = "Weight";
+      const expectedFieldValue = "145";
+      renderWithProviders(<CreatePokemonForm />);
+
+      const weightField = screen.getByLabelText(weightLabel);
+
+      await waitFor(
+        async () => await userEvent.type(weightField, expectedFieldValue)
+      );
+
+      expect(weightField).toHaveValue(expectedFieldValue);
+    });
+  });
+
+  describe("When the user writes on base exp's field", () => {
+    test("Then it should change the value of base exp's field", async () => {
+      const baseExpLabel = "Base exp";
+      const expectedFieldValue = "200";
+      renderWithProviders(<CreatePokemonForm />);
+
+      const baseExpField = screen.getByLabelText(baseExpLabel);
+
+      await waitFor(
+        async () => await userEvent.type(baseExpField, expectedFieldValue)
+      );
+
+      expect(baseExpField).toHaveValue(expectedFieldValue);
+    });
+  });
+
+  describe("When the user ataches a file 'pokemon.png' on image input", () => {
+    test("Then it should change the value of image's field", async () => {
+      const inputLabel = "Image";
+
+      renderWithProviders(<CreatePokemonForm />);
+
+      const testFile = new File(["pokemon"], "pokemon.png", {
+        type: "image/png",
+      });
+
+      const imageInput: HTMLInputElement = screen.getByLabelText(inputLabel);
+
+      fireEvent.change(imageInput, { target: { files: [testFile] } });
+
+      expect(imageInput.files![0]).toStrictEqual(testFile);
     });
   });
 });
