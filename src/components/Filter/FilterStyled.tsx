f44abc045d83a75d29cbde9bd852c09b79bee3ac
Cristina <cristinajmartin98@gmail.com>
2023-03-21 17:09:27 +0100

    Add styles to filter component

---
+++ src/components/Filter/FilterStyled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const FilterStyled = styled.div`
+  padding: 1rem;
+  font-weight: ${(props) => props.theme.fonts.weights.semiBold};
+`;
+
+export default FilterStyled;
