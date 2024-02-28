563fbe020838c737938959c2890e39ac4ef46598
Cristina Jiménez <cristinajm98@hotmail.com>
2024-02-28 15:53:29 +0100

    Change NavBar position to absolute

--- src/__tests__/components/RegisterForm.test.tsx
+++ src/__tests__/components/RegisterForm.test.tsx
@@ -1,4 +1,4 @@
-import { screen, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import renderWithProviders from "../../utils/testUtils/renderWithProviders";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 import userEvent from "@testing-library/user-event";
