39dc0ae7a88880c90b1d2ddcb5950462f4e780b9
Cristina Jiménez <cristinajm98@hotmail.com>
2024-05-09 22:05:30 +0200

    Add create pokemon button on your pokemon page

--- src/styles/shared/PageStyled.ts
+++ src/styles/shared/PageStyled.ts
@@ -12,6 +12,17 @@ const PageStyled = styled.section`
     font-size: ${(props) => props.theme.fonts.sizes.pokemonDescriptionSize};
   }
 
+  .create-pokemon-link {
+    margin: 0 auto;
+    margin-top: ${(props) => props.theme.paddings.medium};
+    font-weight: ${(props) => props.theme.fonts.weights.semiBold};
+    border-radius: ${(props) => props.theme.border.radius};
+    padding: 1rem;
+    color: ${(props) => props.theme.fonts.colors.white};
+    background-color: ${(props) => props.theme.colors.accentColor};
+    box-shadow: ${(props) => props.theme.colors.accentColor} 0px 6px 12px -2px;
+  }
+
   h2 {
     font-size: ${(props) => props.theme.fonts.sizes.bigSize};
   }
